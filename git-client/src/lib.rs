bed906992dcd92868225c5d848749d65540dbdcf
QuentinN42 <quentin@lieumont.fr>
2023-02-04 18:37:50 -0800

    docs: uses triple / to mark some comments as function doc
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- git-client/src/lib.rs
+++ git-client/src/lib.rs
@@ -8,15 +8,15 @@ use git2::{Repository, Error, Delta};
 static DELTA_WHITELIST: &'static [Delta] = &[Delta::Modified, Delta::Added];
 
 
-// Uses git2 to get the list of changed files
-//
-// As git2 is a library around the libgit2 C library,
-// it can raises some errors. Like if the current working
-// directory is not a git repository, or if the git repository
-// is corrupted.
-//
-// https://libgit2.github.com/
-// https://docs.rs/git2/latest/git2/
+/// Uses git2 to get the list of changed files
+///
+/// As git2 is a library around the libgit2 C library,
+/// it can raises some errors. Like if the current working
+/// directory is not a git repository, or if the git repository
+/// is corrupted.
+///
+/// https://libgit2.github.com/
+/// https://docs.rs/git2/latest/git2/
 pub fn get_changed_files() -> Result<Vec<String>, Error> {
     let repo = get_repo()?;
     log::debug!("Successfuly using repo at path : {:?}", repo.path());
@@ -28,16 +28,16 @@ pub fn get_changed_files() -> Result<Vec<String>, Error> {
 // privates functions
 // ------------------
 
-// Repository::discover will try to locate the git repository
-// starting from the working directory and going up to the root
-// If no directory is found, it will return an error.
+/// Repository::discover will try to locate the git repository
+/// starting from the working directory and going up to the root
+/// If no directory is found, it will return an error.
 fn get_repo() -> Result<Repository, Error> {
     Repository::discover(Path::new("."))
 }
 
-// Returns the list of files that have a status in the DELTA_WHITELIST
-//
-// https://docs.rs/git2/latest/git2/enum.Delta.html
+/// Returns the list of files that have a status in the DELTA_WHITELIST
+///
+/// https://docs.rs/git2/latest/git2/enum.Delta.html
 fn get_files_list(repo: Repository) -> Result<Vec<String>, Error> {
     let statuses = repo.statuses(None).unwrap();
     let mut modified_files: Vec<String> = Vec::new();
