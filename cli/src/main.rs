3cca4e9cad250fd6e8c2fae5939fb99d9e775739
QuentinN42 <quentin@lieumont.fr>
2023-04-01 12:56:41 -0700

    feat: early return false
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -16,18 +16,19 @@ fn run_hooks() -> bool {
     let steps_collections = walker::walk();
 
     for steps_collection in steps_collections {
-        log::info!("{:?}", steps_collection);
-
-        let pre_command = match steps_collection.pre_command {
-            Some(x) => x + " && ",
-            None => "".to_string(),
-        };
-
+        log::info!("Running hooks in `{}`", steps_collection.cwd);
         for step in steps_collection.steps {
-            let final_command = pre_command.clone() + &step.command;
-            let cwd = Path::new(&steps_collection.cwd);
-            log::info!("{:?}", final_command);
-            runner::execute(&final_command, cwd);
+            let final_command = match steps_collection.pre_command.as_ref() {
+                Some(pre) => format!("{} && {}", pre, step.command),
+                None => step.command,
+            };
+            log::info!("\tRunning command: `{}`", final_command);
+            if !runner::execute(&final_command, Path::new(&steps_collection.cwd)) {
+                // here the command has failed
+                // TODO: think about a proper way to make it working with parallel executions
+                // early return false for the moment
+                return false;
+            }
         }
     }
     true
